8bdaec64960cd093cebeae574bd971b44642756f
ekarpp <eetu.karppinen@aalto.fi>
2022-11-12 22:49:42 +0200

    start work, just macros and gf left to do

--- src/packed_fmatrix.hh
+++ src/packed_fmatrix.hh
@@ -10,13 +10,19 @@
 #include "gf.hh"
 #include "fmatrix.hh"
 
-typedef long long long4_t __attribute__ ((vector_size (32)));
+
 
 #ifndef PAR
 #define PAR 0
 #endif
 
+#ifdef AVX512
+#define VECTOR_N 32
+typedef long long avx_t __attribute__ ((vector_size (64)));
+#else
 #define VECTOR_N 16
+typedef long long avx_t __attribute__ ((vector_size (32)));
+#endif
 
 #define MOVE_ID_VEC(index)                              \
     {                                                   \
@@ -36,7 +42,7 @@ typedef long long long4_t __attribute__ ((vector_size (32)));
 #define ELIM_LOOP(index)                                            \
         for (int row = r0 + 1; row < this->rows; row++)             \
         {                                                           \
-            long4_t val = _mm256_permutevar8x32_epi32(              \
+            avx_t val = _mm256_permutevar8x32_epi32(              \
                 this->get(row, col),                                \
                 idx                                                 \
             );                                                      \
@@ -58,9 +64,9 @@ typedef long long long4_t __attribute__ ((vector_size (32)));
 #define DET_LOOP(index)                                         \
     {                                                           \
         int r0 = VECTOR_N*col + index;                          \
-        long4_t mx;                                             \
+        avx_t mx;                                             \
         int mxi = -1;                                           \
-        long4_t cmpmsk = _mm256_set_epi64x(                     \
+        avx_t cmpmsk = _mm256_set_epi64x(                     \
             global::F.get_mask() << 48,                         \
             0,                                                  \
             0,                                                  \
@@ -95,7 +101,7 @@ typedef long long long4_t __attribute__ ((vector_size (32)));
         mx_ext = ff_util::ext_euclid(mx_ext);                   \
         this->mul_row(r0, mx_ext);                              \
         char mask = (VECTOR_N - 1 - index) / 2;                 \
-        long4_t idx = _mm256_set1_epi32(mask);                  \
+        avx_t idx = _mm256_set1_epi32(mask);                  \
         /* vectorize end? */                                    \
         if (PAR) {                                              \
             _Pragma("omp parallel for")                         \
@@ -112,14 +118,14 @@ private:
     int cols;
     // original matrix n moduloe VECTOR_N
     int nmod;
-    std::vector<long4_t> m;
+    std::vector<avx_t> m;
 
-    long4_t get(int row, int col) const
+    avx_t get(int row, int col) const
     {
         return this->m[row*this->cols + col];
     }
 
-    void set(int row, int col, long4_t v)
+    void set(int row, int col, avx_t v)
     {
         this->m[row*this->cols + col] = v;
     }
@@ -143,7 +149,11 @@ public:
         {
             for (int c = 0; c < matrix.get_n() / VECTOR_N; c++)
                 this->set(r, c,
+#ifdef AVX512
+                          _mm512_set_epi16(
+#else
                           _mm256_set_epi16(
+#endif
                               matrix(r, VECTOR_N*c +  0).get_repr(),
                               matrix(r, VECTOR_N*c +  1).get_repr(),
                               matrix(r, VECTOR_N*c +  2).get_repr(),
@@ -159,8 +169,27 @@ public:
                               matrix(r, VECTOR_N*c + 12).get_repr(),
                               matrix(r, VECTOR_N*c + 13).get_repr(),
                               matrix(r, VECTOR_N*c + 14).get_repr(),
-                              matrix(r, VECTOR_N*c + 15).get_repr()
-                          )
+#ifndef AVX512
+                              matrix(r, VECTOR_N*c + 15).get_repr())
+#else
+                              matrix(r, VECTOR_N*c + 15).get_repr(),
+                              matrix(r, VECTOR_N*c + 16).get_repr(),
+                              matrix(r, VECTOR_N*c + 17).get_repr(),
+                              matrix(r, VECTOR_N*c + 18).get_repr(),
+                              matrix(r, VECTOR_N*c + 19).get_repr(),
+                              matrix(r, VECTOR_N*c + 20).get_repr(),
+                              matrix(r, VECTOR_N*c + 21).get_repr(),
+                              matrix(r, VECTOR_N*c + 22).get_repr(),
+                              matrix(r, VECTOR_N*c + 23).get_repr(),
+                              matrix(r, VECTOR_N*c + 24).get_repr(),
+                              matrix(r, VECTOR_N*c + 25).get_repr(),
+                              matrix(r, VECTOR_N*c + 26).get_repr(),
+                              matrix(r, VECTOR_N*c + 27).get_repr(),
+                              matrix(r, VECTOR_N*c + 28).get_repr(),
+                              matrix(r, VECTOR_N*c + 29).get_repr(),
+                              matrix(r, VECTOR_N*c + 30).get_repr(),
+                              matrix(r, VECTOR_N*c + 31).get_repr())
+#endif
                     );
             if (this->nmod)
             {
@@ -172,18 +201,49 @@ public:
                     elems[i] = matrix(r, VECTOR_N*c + i).get_repr();
 
                 this->set(r, c,
+#ifdef AVX512
+                          _mm512_set_epi16(
+#else
                           _mm256_set_epi16(
+#endif
                               elems[0],  elems[1],  elems[2],  elems[3],
                               elems[4],  elems[5],  elems[6],  elems[7],
                               elems[8],  elems[9],  elems[10], elems[11],
-                              elems[12], elems[13], elems[14], elems[15]
-                          )
+#ifndef AVX512
+                              elems[12], elems[13], elems[14], elems[15])
+#else
+                              elems[12], elems[13], elems[14], elems[15],
+                              elems[16], elems[17], elems[18], elems[19],
+                              elems[20], elems[21], elems[22], elems[23],
+                              elems[24], elems[25], elems[26], elems[27],
+                              elems[28], elems[29], elems[30], elems[31])
+#endif
+
                 );
             }
         }
 
+#ifdef AVX512
+        avx_t onepos = _mm512_set_epi64(1ull << 48, 0, 0, 0,
+                                        0, 0, 0, 0);
+        const avx_t permute_idx = _mm512_set_epi16(
+            0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111, 0b000,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        );
+        for (int i = 0; i < this->nmod; i++)
+            onepos = _mm512_permutexvar_epi16(permute_idx, onepos);
+
+        for (int r = matrix.get_n(); r < this->rows; r++)
+        {
+            for (int c = 0; c < this->cols - 1; c++)
+                this->set(r, c, _mm512_setzero_si512());
+            this->set(r, this->cols - 1, onepos);
+            onepos = _mm512_permutexvar_epi16(permute_idx, onepos);
+        }
+#else
         /* cycle all one position to right */
-        const long4_t permute_idx = _mm256_set_epi32(
+        const avx_t permute_idx = _mm256_set_epi32(
             0b000,
             0b111,
             0b110,
@@ -194,7 +254,7 @@ public:
             0b001
         );
 
-        long4_t onepos = _mm256_set_epi64x(1ull << 48, 0, 0, 0);
+        avx_t onepos = _mm256_set_epi64x(1ull << 48, 0, 0, 0);
         for (int i = 0; i < this->nmod; i++)
             MOVE_ID_VEC(i);
 
@@ -205,11 +265,12 @@ public:
             this->set(r, this->cols - 1, onepos);
             MOVE_ID_VEC(r % VECTOR_N);
         }
+#endif
     }
 
     void swap_rows(int r1, int r2)
     {
-        long4_t tmp;
+        avx_t tmp;
         for (int c = 0; c < this->cols; c++)
         {
             tmp = this->get(r1, c);
@@ -220,7 +281,11 @@ public:
 
     void mul_row(int row, uint64_t v)
     {
-        long4_t pack = _mm256_set1_epi16(v);
+#ifdef AVX512
+        avx_t pack = _mm512_set1_epi16(v);
+#else
+        avx_t pack = _mm256_set1_epi16(v);
+#endif
         for (int col = 0; col < this->cols; col++)
             this->set(row, col,
                       ff_util::wide_mul(this->get(row, col), pack)
@@ -228,14 +293,18 @@ public:
     }
 
     /* subtract v times r1 from r2 */
-    void row_op(int r1, int r2, long4_t pack)
+    void row_op(int r1, int r2, avx_t pack)
     {
         for (int col = 0; col < this->cols; col++)
         {
-            long4_t tmp = ff_util::wide_mul(this->get(r1, col), pack);
+            avx_t tmp = ff_util::wide_mul(this->get(r1, col), pack);
 
             this->set(r2, col,
+#ifdef AVX512
+                      _mm512_xor_si512(this->get(r2,col), tmp)
+#else
                       _mm256_xor_si256(this->get(r2, col), tmp)
+#endif
                 );
         }
     }
