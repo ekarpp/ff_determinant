04ed82eaf410cb2aa2758994b5123034c3434a72
Eetu Karppinen <26388170+ekarpp@users.noreply.github.com>
2022-11-12 19:25:57 +0200

    Dense slicing (#1)
    * apply dense slicing to packed init
    
    * fix init, fix mul_row
    
    * applies dense bit slicing to determinant

--- src/packed_fmatrix.hh
+++ src/packed_fmatrix.hh
@@ -10,13 +10,51 @@
 #include "gf.hh"
 #include "fmatrix.hh"
 
-typedef long long int long4_t __attribute__ ((vector_size (32)));
+typedef long long long4_t __attribute__ ((vector_size (32)));
 
 #ifndef PAR
 #define PAR 0
 #endif
 
-#define VECTOR_N 8
+#define MOVE_ID_VEC(index)                              \
+    {                                                   \
+        if (index%2)                                    \
+            onepos = _mm256_slli_epi32(                 \
+                _mm256_permutevar8x32_epi32(            \
+                    onepos,                             \
+                    permute_idx                         \
+                ),                                      \
+                16                                      \
+            );                                          \
+        else                                            \
+            onepos = _mm256_srli_epi32(onepos, 16);     \
+    }
+
+
+#define ELIM_LOOP(index)                                            \
+        for (int row = r0 + 1; row < this->rows; row++)             \
+        {                                                           \
+            long4_t val = _mm256_permutevar8x32_epi32(              \
+                this->get(row, col),                                \
+                idx                                                 \
+            );                                                      \
+            if (index % 2)                                          \
+                val = _mm256_blend_epi16(                           \
+                    val,                                            \
+                    _mm256_slli_epi32(val, 16),                     \
+                    0xAA                                            \
+                );                                                  \
+            else                                                    \
+                val = _mm256_blend_epi16(                           \
+                    _mm256_srli_epi32(val, 16),                     \
+                    val,                                            \
+                    0xAA                                            \
+                );                                                  \
+            this->row_op(r0, row, val);                             \
+        }
+
+
+#define VECTOR_N 16
 
 #define DET_LOOP(index)                                         \
     {                                                           \
@@ -24,14 +62,14 @@ typedef long long int long4_t __attribute__ ((vector_size (32)));
         long4_t mx;                                             \
         int mxi = -1;                                           \
         long4_t cmpmsk = _mm256_set_epi64x(                     \
-            0xFFFFull << 32,                                    \
+            global::F.get_mask() << 48,                         \
             0,                                                  \
             0,                                                  \
             0                                                   \
         );                                                      \
-        if (index >= 4)                                         \
+        if (index >= (VECTOR_N / 2))                            \
             cmpmsk = _mm256_permute4x64_epi64(cmpmsk, 0x0C);    \
-        cmpmsk = _mm256_srli_si256(cmpmsk, 4*(index%4));        \
+        cmpmsk = _mm256_srli_si256(cmpmsk, 2 * (index % 8));    \
         for (int row = r0; row < this->rows; row++)             \
         {                                                       \
             char ZF = _mm256_testz_si256(                       \
@@ -48,7 +86,7 @@ typedef long long int long4_t __attribute__ ((vector_size (32)));
         if (mxi == -1)                                          \
             return global::F.zero();                            \
         uint64_t mx_ext =                                       \
-            _mm256_extract_epi32(mx, VECTOR_N - 1 - index);     \
+            _mm256_extract_epi16(mx, VECTOR_N - 1 - index);     \
         if (mxi != r0)                                          \
             this->swap_rows(mxi, r0);                           \
         /* vectorize? */                                        \
@@ -57,28 +95,14 @@ typedef long long int long4_t __attribute__ ((vector_size (32)));
         );                                                      \
         mx_ext = ff_util::ext_euclid(mx_ext);                   \
         this->mul_row(r0, mx_ext);                              \
-        /* vectorize end? */                                    \
-        char mask = VECTOR_N - 1 - index;                       \
+        char mask = (VECTOR_N - 1 - index) / 2;                 \
         long4_t idx = _mm256_set1_epi32(mask);                  \
+        /* vectorize end? */                                    \
         if (PAR) {                                              \
             _Pragma("omp parallel for")                         \
-            for (int row = r0 + 1; row < this->rows; row++)     \
-            {                                                   \
-                long4_t val = _mm256_permutevar8x32_epi32(      \
-                    this->get(row, col),                        \
-                    idx                                         \
-                );                                              \
-                this->row_op(r0, row, val);                     \
-            }                                                   \
+            ELIM_LOOP(index);                                   \
         } else {                                                \
-            for (int row = r0 + 1; row < this->rows; row++)     \
-            {                                                   \
-                long4_t val = _mm256_permutevar8x32_epi32(      \
-                    this->get(row, col),                        \
-                    idx                                         \
-                );                                              \
-                this->row_op(r0, row, val);                     \
-            }                                                   \
+            ELIM_LOOP(index);                                   \
         }                                                       \
     }
 
@@ -120,15 +144,23 @@ public:
         {
             for (int c = 0; c < matrix.get_n() / VECTOR_N; c++)
                 this->set(r, c,
-                          _mm256_set_epi32(
-                              matrix(r, VECTOR_N*c + 0).get_repr(),
-                              matrix(r, VECTOR_N*c + 1).get_repr(),
-                              matrix(r, VECTOR_N*c + 2).get_repr(),
-                              matrix(r, VECTOR_N*c + 3).get_repr(),
-                              matrix(r, VECTOR_N*c + 4).get_repr(),
-                              matrix(r, VECTOR_N*c + 5).get_repr(),
-                              matrix(r, VECTOR_N*c + 6).get_repr(),
-                              matrix(r, VECTOR_N*c + 7).get_repr()
+                          _mm256_set_epi16(
+                              matrix(r, VECTOR_N*c +  0).get_repr(),
+                              matrix(r, VECTOR_N*c +  1).get_repr(),
+                              matrix(r, VECTOR_N*c +  2).get_repr(),
+                              matrix(r, VECTOR_N*c +  3).get_repr(),
+                              matrix(r, VECTOR_N*c +  4).get_repr(),
+                              matrix(r, VECTOR_N*c +  5).get_repr(),
+                              matrix(r, VECTOR_N*c +  6).get_repr(),
+                              matrix(r, VECTOR_N*c +  7).get_repr(),
+                              matrix(r, VECTOR_N*c +  8).get_repr(),
+                              matrix(r, VECTOR_N*c +  9).get_repr(),
+                              matrix(r, VECTOR_N*c + 10).get_repr(),
+                              matrix(r, VECTOR_N*c + 11).get_repr(),
+                              matrix(r, VECTOR_N*c + 12).get_repr(),
+                              matrix(r, VECTOR_N*c + 13).get_repr(),
+                              matrix(r, VECTOR_N*c + 14).get_repr(),
+                              matrix(r, VECTOR_N*c + 15).get_repr()
                           )
                     );
             if (this->nmod)
@@ -141,11 +173,11 @@ public:
                     elems[i] = matrix(r, VECTOR_N*c + i).get_repr();
 
                 this->set(r, c,
-                          _mm256_set_epi32(
-                              elems[0], elems[1],
-                              elems[2], elems[3],
-                              elems[4], elems[5],
-                              elems[6], elems[7]
+                          _mm256_set_epi16(
+                              elems[0],  elems[1],  elems[2],  elems[3],
+                              elems[4],  elems[5],  elems[6],  elems[7],
+                              elems[8],  elems[9],  elems[10], elems[11],
+                              elems[12], elems[13], elems[14], elems[15]
                           )
                 );
             }
@@ -163,22 +195,16 @@ public:
             0b001
         );
 
-        long4_t onepos = _mm256_set_epi64x(1ull << 32, 0, 0, 0);
+        long4_t onepos = _mm256_set_epi64x(1ull << 48, 0, 0, 0);
         for (int i = 0; i < this->nmod; i++)
-            onepos = _mm256_permutevar8x32_epi32(
-                onepos,
-                permute_idx
-            );
+            MOVE_ID_VEC(i);
 
         for (int r = matrix.get_n(); r < this->rows; r++)
         {
             for (int c = 0; c < this->cols - 1; c++)
                 this->set(r, c, _mm256_setzero_si256());
             this->set(r, this->cols - 1, onepos);
-            onepos = _mm256_permutevar8x32_epi32(
-                onepos,
-                permute_idx
-            );
+            MOVE_ID_VEC(r % VECTOR_N);
         }
     }
 
@@ -195,7 +221,7 @@ public:
 
     void mul_row(int row, uint64_t v)
     {
-        long4_t pack = _mm256_set1_epi32(v);
+        long4_t pack = _mm256_set1_epi16(v);
         for (int col = 0; col < this->cols; col++)
             this->set(row, col,
                       ff_util::wide_mul(this->get(row, col), pack)
